57babd49da078d2614c4753f822efd2f51bab648
pranav-two <pranav@two.ai>
2024-03-20 08:41:30 +0900

    added atoms for sutra and other

--- src/state/atoms.ts
+++ src/state/atoms.ts
@@ -12,11 +12,15 @@ const sutraModelAtom = atom<SutraModel>(SUTRA_MODELS[0]);
 const sutraTemperatureAtom = atom(SUTRA_MODELS[0].temperature);
 const sutraMaxTokensAtom = atom(SUTRA_MODELS[0].maxTokens);
 const sutraStatsAtom = atom<SutraStats>(initStats());
+const sutraLoadingAtom = atom(false);
+const sutraAnswerAtom = atom('');
 
 const otherModelAtom = atom<SutraModel>(OTHER_MODELS[0]);
 const otherTemperatureAtom = atom(OTHER_MODELS[0].temperature);
 const otherMaxTokensAtom = atom(OTHER_MODELS[0].maxTokens);
 const otherStatsAtom = atom<SutraStats>(initStats());
+const otherLoadingAtom = atom(false);
+const otherAnswerAtom = atom('');
 
 export {
     agentInfoAtom,
@@ -27,9 +31,13 @@ export {
     sutraTemperatureAtom,
     sutraMaxTokensAtom,
     sutraStatsAtom,
+    sutraLoadingAtom,
+    sutraAnswerAtom,
 
     otherModelAtom,
     otherTemperatureAtom,
     otherMaxTokensAtom,
     otherStatsAtom,
+    otherLoadingAtom,
+    otherAnswerAtom,
 };
