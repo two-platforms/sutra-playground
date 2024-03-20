08dafdf0776b0c68cd38b93abcde7ecd1601d814
pranav-two <pranav@two.ai>
2024-03-20 22:01:37 +0900

    minor UI stats

--- src/components/StatsView.tsx
+++ src/components/StatsView.tsx
@@ -1,6 +1,7 @@
 import { Chip, Tooltip } from '@nextui-org/react';
 import { useAtom, PrimitiveAtom } from 'jotai';
 import { SutraStats } from '../service/SutraModels';
+import { DashboardDots } from 'iconoir-react';
 
 interface Props {
   statsAtom: PrimitiveAtom<SutraStats>;
@@ -10,8 +11,27 @@ interface Props {
 export const StatsView = ({ statsAtom }: Props) => {
   const [stats] = useAtom(statsAtom);
   return (
-    <div className="font-mono text-sm">
-      <Chip size="md" color="primary">
+    <div className="flex flex-row items-center gap-2 font-mono text-sm">
+      <Tooltip content={stats.enTranslation} placement="top" className=" max-w-96">
+        <DashboardDots />
+      </Tooltip>
+      |
+      <div>
+        <b>{stats.tps.toFixed(2)}</b> TOKENS/SEC
+      </div>
+      |
+      <div>
+        <b>{stats.tokenCount}</b> TOKENS
+      </div>
+      |
+      <div>
+        <b>{stats.ttftService}</b>ms 1ST TOKEN
+      </div>
+      |
+      <div>
+        <b>{stats.ttltService}</b>ms
+      </div>
+      {/* <Chip size="md" color="primary">
         {stats.tps.toFixed(2)} Tokens/Sec
       </Chip>
       -<Chip size="md">{stats.tokenCount} Tokens</Chip>-<Chip size="md">{stats.ttftService}ms TTFB</Chip>-
@@ -20,7 +40,7 @@ export const StatsView = ({ statsAtom }: Props) => {
       </Chip>
       <Tooltip content={stats.enTranslation} placement="top" className=' max-w-96'>
       <Chip size="md">E</Chip>
-      </Tooltip>
+      </Tooltip> */}
       {/* TEMPERATURE: <Chip size='sm'>{temperature}</Chip>
       MAX TOKENS: <Chip size='sm'>{maxTokens}</Chip> */}
     </div>
