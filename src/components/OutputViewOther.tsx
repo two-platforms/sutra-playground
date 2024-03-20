536c08bfe6727cb0d5042c87673091ab2ff0335a
simon-two <simon@two.ai>
2024-03-20 07:37:25 -0700

    tokens/sec using service times instead of client times

--- src/components/OutputViewOther.tsx
+++ src/components/OutputViewOther.tsx
@@ -45,7 +45,7 @@ export function OutputViewOther() {
     },
     onLLMReply: (v: LLMReply) => {
       const ttlt = Date.now() - timerStart;
-      const tps = (1000 * v.tokenCount) / (ttlt - stats.ttftClient);
+      const tps = (1000 * v.tokenCount) / (v.ttltMsec - v.ttftMsec);
       const newStats = {
         ...stats,
         ttltClient: ttlt,
